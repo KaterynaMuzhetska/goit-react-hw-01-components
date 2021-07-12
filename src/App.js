e43a6b310175019940cb69abc1659cb06a6a0130
KaterynaMuzhetska <74104436+KaterynaMuzhetska@users.noreply.github.com>
2021-07-12 22:54:01 +0300

    HW01-task2

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import React from "react";
 import SocialProfile from "./SocialProfile/SocialProfile.js";
 import user from "./SocialProfile/user.json";
 
+import Statistics from "./components/statistics/Statistics";
+import statisticalData from "./components/statistics/statistical-data.json";
+
 function App() {
   return (
     <div className="App">
@@ -15,6 +18,8 @@ function App() {
         views={user.stats.views}
         likes={user.stats.likes}
       />
+      <Statistics title="Upload stats" stats={statisticalData} />
+      <Statistics stats={statisticalData} />
     </div>
   );
 }
